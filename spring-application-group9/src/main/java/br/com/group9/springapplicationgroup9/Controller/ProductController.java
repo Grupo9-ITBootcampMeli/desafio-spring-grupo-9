4931374fc537403368e65a6f97596dd082088912
Guilherme da Silva <guilherme.pereira@mercadolivre.com>
2022-03-24 15:35:50 -0300

    create routes POST and GET

--- spring-application-group9/src/main/java/br/com/group9/springapplicationgroup9/Controller/ProductController.java
+++ spring-application-group9/src/main/java/br/com/group9/springapplicationgroup9/Controller/ProductController.java
@@ -1,4 +1,29 @@
 package br.com.group9.springapplicationgroup9.Controller;
 
+import br.com.group9.springapplicationgroup9.Entity.Product;
+import br.com.group9.springapplicationgroup9.Service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping("/v1/insert-articles")
+    public ResponseEntity<String> insertArticles(@RequestBody List<Product> product){
+        productService.registerProducts(product);
+        return ResponseEntity.ok("OK");
+    }
+
+    @GetMapping("/v1/articles")
+    public ResponseEntity<String> getArticles(@RequestParam Map<String, String> params, @RequestParam (name = "order", required = false) String order) {
+        return ResponseEntity.ok(productService.listProducts(params).toString());
+    }
+
 }
