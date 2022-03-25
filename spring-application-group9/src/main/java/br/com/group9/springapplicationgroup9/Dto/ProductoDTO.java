325a810421ae7353739028e532d156720c78ac03
Vinicius Clemente Brito <vinicius.brito@mercadolivre.com>
2022-03-25 09:53:47 -0300

    Revert "Features - 01 ao 04"

--- spring-application-group9/src/main/java/br/com/group9/springapplicationgroup9/Dto/ProductoDTO.java
+++ spring-application-group9/src/main/java/br/com/group9/springapplicationgroup9/Dto/ProductoDTO.java
@@ -1,39 +1,4 @@
 package br.com.group9.springapplicationgroup9.Dto;
 
-import br.com.group9.springapplicationgroup9.Entity.Product;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.stream.Collectors;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-// DTO para o endpoint de GET
 public class ProductoDTO {
-
-    private Long productId;
-    private String name;
-    private BigDecimal price;
-    private Boolean freeShipping;
-    private String prestige;
-
-    public ProductoDTO converte(Product product) {
-        this.productId = product.getProductId();
-        this.name = product.getName();
-        this.price = product.getPrice();
-        this.freeShipping = product.getFreeShipping();
-        this.prestige = product.getPrestige();
-        return this;
-    }
-
-    public static List<ProductoDTO> converte(List<Product> products) {
-        return products.stream().map(
-                p -> new ProductoDTO(p.getProductId(), p.getName(), p.getPrice(), p.getFreeShipping(), p.getPrestige())
-        ).collect(Collectors.toList());
-    }
 }
--- spring-application-group9/src/main/java/br/com/group9/springapplicationgroup9/Util/Interfaces/IJsonHandler.java
+++
@@ -1,9 +0,0 @@
-package br.com.group9.springapplicationgroup9.Util.Interfaces;
-
-import java.util.List;
-
-public interface IJsonHandler<T>{
-    String DEFAULT_PATH = "src/main/java/br/com/group9/springapplicationgroup9/Repository/";
-    List<T> read();
-    boolean save(List<T> list);
-}
